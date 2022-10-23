6f599cfec2dbf4ba7bfc1cb6f6fd308d3d84b6ed
Alison Lovegood <alisondlovegood@gmail.com>
2022-10-23 19:30:43 -0300

    Conceitos REST, primeiro endpoint de cadastro

---
+++ src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.cloudparking.controller;
+
+import one.digitalinnovation.cloudparking.model.Parking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    @GetMapping
+    public List<Parking> findAll(){
+        var parking = new Parking();
+        parking.setColor("black");
+        parking.setLicense("KIZ-8098");
+        parking.setModel("corsa");
+        parking.setState("PE");
+
+        return List.of(parking);
+    }
+
+}
