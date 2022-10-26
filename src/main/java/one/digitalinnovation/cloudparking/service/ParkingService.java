4116f6c2ea0aaaf9d6b368e3c654d68079d87b9c
Alison Lovegood <alisondlovegood@gmail.com>
2022-10-26 09:56:42 -0300

    Criando interface repository e alterando service

--- src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.cloudparking.service;
 
 import one.digitalinnovation.cloudparking.exception.ParkingNotFoundException;
 import one.digitalinnovation.cloudparking.model.Parking;
+import one.digitalinnovation.cloudparking.repository.ParkingRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,52 +14,46 @@ import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
-//    como nao ha bd ainda, criei uma lista de dados
-    private static Map<String, Parking> parkingMap = new HashMap();
-//    static {
-//        var id = getUUID();
-//        var id1 = getUUID();
-//        Parking parking = new Parking(id, "KIZ-8909", "PE", "CORSA", "CINZA");
-//        Parking parking1 = new Parking(id1, "KIZ-8990", "PE", "CORSA", "PRETO");
-//        parkingMap.put(id, parking);
-//        parkingMap.put(id1, parking1);
-//    }
 
-    public List<Parking> findAll(){
-        return parkingMap.values().stream().collect(Collectors.toList());
+    private final ParkingRepository parkingRepository;
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
     }
 
-//    nao fazer com que o bd gere automaticamente a chave, por padrao utiliza-se uma chave gerada pelo uuid do java
+    private static Map<String, Parking> parkingMap = new HashMap();
+
+    public List<Parking> findAll(){
+//        substituindo codigo antigo pelo novo do repositorio
+        return parkingRepository.findAll();
+//        return parkingMap.values().stream().collect(Collectors.toList());
+    }
+
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
     public Parking findById(String id) {
-        Parking parking = parkingMap.get(id);
-        if (parking == null){
-            throw new ParkingNotFoundException(id);
-        }
-        return parking;
+        return parkingRepository.findById(id).orElseThrow(()->
+                new ParkingNotFoundException(id));
     }
 
     public Parking create(Parking parkingCreate) {
         String uuid = getUUID();
         parkingCreate.setId(uuid);
         parkingCreate.setEntryDate(LocalDateTime.now());
-        parkingMap.put(uuid, parkingCreate);
+        parkingRepository.save(parkingCreate);
         return parkingCreate;
     }
 
     public void delete(String id) {
-//        se nao existir ja entra na exception
         findById(id);
-        parkingMap.remove(id);
+        parkingRepository.deleteById(id);
     }
 
     public Parking update(String id, Parking parkingCreate) {
         Parking byId = findById(id);
         byId.setColor(parkingCreate.getColor());
-        parkingMap.replace(id, byId);
+        parkingRepository.save(byId);
         return byId;
     }
 
